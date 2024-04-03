4e579203765bb58b0ecd607f0d97e21a6d9de58b
Sreelaxmi12 <sreelaxmi0803@gmail.com>
2024-04-03 14:45:17 +0530

    Integrated tailwind

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="text-3xl font-bold underline text-red-900">
+      Hey there!!! My first tailwind css project!
+    </div>
+  );
+};
+
+export default Navbar;
